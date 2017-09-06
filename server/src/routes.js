882c8b7a328f8f463bd87ffe92a110494396436e
Ricardo Dias <slowmotion@outlook.com>
2017-09-06 16:18:41 +0100

    Followed part 2 of the project

---
+++ server/src/routes.js
@@ -0,0 +1,9 @@
+const AuthenticationController = require('./controllers/AuthenticationController')
+
+const AutthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+
+module.exports = (app) => {
+  app.post('/register',
+    AutthenticationControllerPolicy.register,
+    AuthenticationController.register)
+}
