ab30308ebc610eeccd7ab17e28a575fbc5a3d33b
Ricardo Dias <slowmotion@outlook.com>
2017-09-26 15:27:20 +0100

    Made Part 4 of tutorial

--- client/src/components/CreateSong.vue
+++ client/src/components/CreateSong.vue
@@ -5,26 +5,33 @@
         <panel title="Song Metadata">
           <v-text-field
             label="Title"
+            required
+            :rules=[required]
             v-model="song.title">
           </v-text-field>
           <v-text-field
             label="Artist"
+            :rules=[required]
             v-model="song.artist">
           </v-text-field>
           <v-text-field
             label="Genre"
+            :rules=[required]
             v-model="song.genre">
           </v-text-field>
           <v-text-field
             label="Album"
+            :rules=[required]
             v-model="song.album">
           </v-text-field>
           <v-text-field
             label="Album Image Url"
+            :rules=[required]
             v-model="song.albumImageUrl">
           </v-text-field>
           <v-text-field
-            label="Youtube IDl"
+            label="Youtube ID"
+            :rules=[required]
             v-model="song.youtubeId">
           </v-text-field>
         </panel>
@@ -33,17 +40,20 @@
         <panel title="Song Structure" class="ml-2">
           <v-text-field
             label="Tab"
+            :rules=[required]
             v-model="song.tab"
             multi-line>
           </v-text-field>
           <v-text-field
             label="Lyrics"
+            :rules=[required]
             multi-line
             v-model="song.lyrics">
           </v-text-field>
         </panel>
+        <div class="danger-alert" v-if="error">{{error}}</div>
         <v-btn dark class="cyan"
-          @click="create" >Create
+          @click="create" >Create Song
         </v-btn>
       </v-flex>
     </v-layout>
@@ -68,7 +78,9 @@ export default {
         youtubeId: null,
         lyrics: null,
         tab: null
-      }
+      },
+      error: null,
+      required: (value) => !!value || 'Required.'
     }
   },
   components: {
@@ -76,7 +88,14 @@ export default {
   },
   methods: {
     async create () {
-      // call API
+      this.error = null
+      const areAllFieldsFilledIn = Object
+        .keys(this.song)
+        .every(key => !!this.song[key])
+      if (!areAllFieldsFilledIn) {
+        this.error = 'Please fill in all the required fields.'
+        return
+      }
       try {
         await SongsService.post(this.song)
         this.$router.push({name: 'songs'})
--- client/src/components/Songs.vue
+++ client/src/components/Songs.vue
@@ -2,7 +2,45 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <panel title="Songs">
-        <div v-for="song in songs" :key="song.id">
+        <v-btn 
+          slot="action"
+          @click="navigateTo({name: 'songs-create'})"
+          class="cyan accent-2"
+          light medium absolute 
+          right middle
+          fab>
+          <v-icon>add</v-icon>
+        </v-btn>
+
+        <div class="song" v-for="song in songs" 
+          :key="song.id">
+
+          <v-layout>
+            <v-flex xs6>
+              <div class="song-title">
+                {{song.title}}
+              </div>
+              <div class="song-artist">
+                {{song.artist}}
+              </div>
+              <div class="song-genre">
+                {{song.genre}}
+              </div>
+              <v-btn dark class="cyan"
+                @click="navigateTo({
+                  name: 'song',
+                  params: {
+                    songId: song.id
+                  }
+                })" >
+                Song
+              </v-btn>
+            </v-flex>
+
+            <v-flex xs6>
+              <img :src="song.albumImageUrl" class="album-image">
+            </v-flex>
+          </v-layout>
           {{song.title}} - {{song.artist}} - {{song.album}}
         </div>
 
@@ -23,6 +61,11 @@ export default {
       songs: null
     }
   },
+  methods: {
+    navigateTo (route) {
+      this.$router.push(route)
+    }
+  },
   async mounted () {
     // do a request to the backend for all the songs
     this.songs = (await SongsService.index()).data
@@ -31,5 +74,28 @@ export default {
 }
 </script>
 <style scoped>
+.song {
+  padding: 20px;
+  height: 330px;
+  overflow: hidden;
+}
 
+.song-title {
+  font-size: 30px
+
+}
+
+.song-artist {
+  font-size: 24px
+
+}
+
+.song-genre {
+  font-size: 18px
+}
+
+.album-image {
+  width: 70%;
+  margin: 0 auto;
+}
 </style>
